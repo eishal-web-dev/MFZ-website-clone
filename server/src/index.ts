fb5e0aef3bbd0152fd142d5dbfcef1556c8ef553
Eishal <eishal.cecos@gmail.com>
2026-08-09 16:00:37 +0500

    Allow optimized product image uploads

--- server/src/index.ts
+++ server/src/index.ts
@@ -92,10 +92,11 @@ const corsOptions: CorsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 app.use(
   express.urlencoded({
     extended: true,
+    limit: '2mb',
   }),
 );
 
